ba142f5cb5a54182473fcd15ee7a1cfbe262d039
Kim Stephenson <email": null>
2016-08-29 14:28:35 -0700

    Pass all rspec tests

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,17 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.find { |soda| soda.brand == soda_brand }
   end
 
   def sell(soda_brand)
+    soda_to_sell = find_soda(soda_brand)
+    return nil if soda_to_sell == nil
+    @cash += soda_to_sell.price
+    @sodas.delete(soda_to_sell)
   end
-
 end
